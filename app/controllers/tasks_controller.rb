3e3c7c971a49f5301fb845701c16bb4f0eedc578
NaokiNakamichi <ronnao0516@gmail.com>
2022-06-14 15:05:30 +0900

    hoge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,6 +13,24 @@ class TasksController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to root_path
+    else
+      render 'edit', status: unprocessable_entry
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to root_path, status: :see_other
+  end
+
   private
   def task_params
     params.require(:task).permit(:title)
