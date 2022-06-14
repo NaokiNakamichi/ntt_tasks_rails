5e506bb11ef44efbdbc5ab63cf5c6af710863496
NaokiNakamichi <ronnao0516@gmail.com>
2022-06-14 14:39:14 +0900

    hoge

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,17 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def
+    p task_params
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title)
+  end
 end
